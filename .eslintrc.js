af45d7c6ce3b9570378316b130d8be08cbe1fe5d
chengzi-jin <yuyangjunhxy@163.com>
2021-06-08 20:35:36 +0800

    chore: prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,17 +6,17 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended"
-    // "@vue/prettier",
-    // "@vue/prettier/@typescript-eslint",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint',
   ],
   parserOptions: {
     ecmaVersion: 2021,
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
-};
+}
--- README.md
+++ README.md
@@ -2,14 +2,19 @@
 
 git commit message 校验
 
+eslint & prettier
+
+alias
+
+vueRouter
+
 eslint
-
-
-
-
-eslint 
 eslint-plugin-vue
 @vue/eslint-config-typescript
 @typescript-eslint/parser
 @typescript-eslint/eslint-plugin
 typescript
+
+prettier
+eslint-plugin-prettier
+@vue/eslint-config-prettier
